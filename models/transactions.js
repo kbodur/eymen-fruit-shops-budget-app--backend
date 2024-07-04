06461326e47dbbcfc37f8452f629cfbfd358d3bd
kbodur <kbodur@pursuit.org>
2024-07-04 18:32:18 -0400

    I created the unique ID data

---
+++ models/transactions.js
@@ -0,0 +1,54 @@
+const transactionsArray=[
+
+    {
+        id: "8j-vg",
+        item_name: "Apple Sales",
+        amount: 50,
+        date: "2024-06-01",
+        from: "Local Market",
+        category: "Sales" 
+    },
+    {
+        id: "sNqgY",
+        item_name: "Banana Purchase",
+        amount: -30,
+        date: "2024-06-02",
+        from: "Supplier",
+        category: "Purchases"
+    },
+    {
+        id: "5Iq1N",
+        item_name: "Cherry Sales",
+        amount: 75,
+        date: "2024-06-03",
+        from: "Farmers Market",
+        category: "Sales"
+    },
+    {
+        id: "-1dLb",
+        item_name: "Grape Purchase",
+        amount: -45,
+        date: "2024-06-04",
+        from: "Wholesaler",
+        category: "Purchases"
+    },
+      
+    {
+        id: "4gcbW",
+        item_name: "Orange Sales",
+        amount: 60,
+        date: "2024-06-05",
+        from: "Local Market",
+        category: "Sales"
+    },
+    {
+        id:"dtTRj",
+        item_name: "Mango Purchase",
+        amount: -25,
+        date: "2024-06-06",
+        from: "Supplier",
+        category: "Purchases"
+    }
+    
+    ]
+    module.exports= transactionsArray
